9563854ffa20aaa267c49961a93a06f00c75de24
sanketh5 <45683032+sanketh5@users.noreply.github.com>
2022-05-24 00:10:20 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit  } from '@angular/core';
+@Component({
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+title = 'scroll';
+nos=5;
+ngOnInit(): void {}
+list_of_images =['a.webp','b.webp','c.webp','d.webp','e.webp','f.webp','g.webp','h.webp','i.webp','j.webp']
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IvyCarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
